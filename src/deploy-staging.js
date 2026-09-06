a1e5f11facff34515c36cdea818210c6db8b0f4b
David Tennyson <136488774+SteveReporting@users.noreply.github.com>
2026-09-06 23:35:11 +0100

    Use unified setup command in staging deployment

--- src/deploy-staging.js
+++ src/deploy-staging.js
@@ -1,9 +1,6 @@
 import 'dotenv/config';
 import { REST, Routes } from 'discord.js';
 import { data as setupCommand } from './commands/setup.js';
-import { data as setup2Command } from './commands/setup2.js';
-import { data as setup3Command } from './commands/setup3.js';
-import { data as setup4Command } from './commands/setup4.js';
 import { data as modCommand } from './commands/mod.js';
 
 const token = process.env.TOKEN;
@@ -19,7 +16,7 @@ if (!stagingGuildId) {
   process.exit(1);
 }
 
-const commands = [setupCommand, setup2Command, setup3Command, setup4Command, modCommand].map((x) => x.toJSON());
+const commands = [setupCommand, modCommand].map((x) => x.toJSON());
 const rest = new REST({ version: '10' }).setToken(token);
 
 try {
