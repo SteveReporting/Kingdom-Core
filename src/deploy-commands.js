b39866b1a48dde381648c9710c9801e2b73c1da9
David Tennyson <136488774+SteveReporting@users.noreply.github.com>
2026-09-06 23:37:11 +0100

    Deploy setup4 advanced platform migration command

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { REST, Routes } from 'discord.js';
 import { data as setupCommand } from './commands/setup.js';
+import { data as setup4Command } from './commands/setup4.js';
 import { data as modCommand } from './commands/mod.js';
 
 const token = process.env.TOKEN;
@@ -14,6 +15,7 @@ if (!token) {
 
 const commands = [
   setupCommand.toJSON(),
+  setup4Command.toJSON(),
   modCommand.toJSON()
 ];
 const rest = new REST({ version: '10' }).setToken(token);
