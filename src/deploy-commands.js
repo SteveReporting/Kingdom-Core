d355abb5cca36b0fc4747b8e71999d3e3a846de6
David Tennyson <136488774+SteveReporting@users.noreply.github.com>
2026-09-06 16:30:35 +0100

    Deploy setup and unified moderation commands

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { REST, Routes } from 'discord.js';
 import { data as setupCommand } from './commands/setup.js';
+import { data as modCommand } from './commands/mod.js';
 
 const token = process.env.TOKEN;
 const clientId = process.env.CLIENT_ID || '1546171480952283166';
@@ -11,7 +12,7 @@ if (!token) {
   process.exit(1);
 }
 
-const commands = [setupCommand.toJSON()];
+const commands = [setupCommand.toJSON(), modCommand.toJSON()];
 const rest = new REST({ version: '10' }).setToken(token);
 
 try {
