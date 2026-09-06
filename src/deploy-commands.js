33b9238d58754a8a2bd4815477904feba7b53b93
David Tennyson <136488774+SteveReporting@users.noreply.github.com>
2026-09-06 19:04:48 +0100

    Deploy setup3 command

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { REST, Routes } from 'discord.js';
 import { data as setupCommand } from './commands/setup.js';
 import { data as setup2Command } from './commands/setup2.js';
+import { data as setup3Command } from './commands/setup3.js';
 import { data as modCommand } from './commands/mod.js';
 
 const token = process.env.TOKEN;
@@ -13,7 +14,12 @@ if (!token) {
   process.exit(1);
 }
 
-const commands = [setupCommand.toJSON(), setup2Command.toJSON(), modCommand.toJSON()];
+const commands = [
+  setupCommand.toJSON(),
+  setup2Command.toJSON(),
+  setup3Command.toJSON(),
+  modCommand.toJSON()
+];
 const rest = new REST({ version: '10' }).setToken(token);
 
 try {
