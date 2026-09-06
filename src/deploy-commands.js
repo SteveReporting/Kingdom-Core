73547df32593842cbe0cf397e8c4f98a562a90f5
David Tennyson <136488774+SteveReporting@users.noreply.github.com>
2026-09-07 00:48:40 +0100

    Deploy new setup5 command

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { REST, Routes } from 'discord.js';
 import { data as setupCommand } from './commands/setup.js';
+import { data as setup5Command } from './commands/setup5.js';
 import { data as modCommand } from './commands/mod.js';
 
 const token = process.env.TOKEN;
@@ -14,6 +15,7 @@ if (!token) {
 
 const commands = [
   setupCommand.toJSON(),
+  setup5Command.toJSON(),
   modCommand.toJSON()
 ];
 const rest = new REST({ version: '10' }).setToken(token);
