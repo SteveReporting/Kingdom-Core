dd52ec3a1212a05345d97c8c7ac78b66c6401e35
David Tennyson <136488774+SteveReporting@users.noreply.github.com>
2026-09-07 23:18:14 +0100

    Deploy setup2 instead of setup5 and setup10 to staging

--- src/deploy-staging.js
+++ src/deploy-staging.js
@@ -1,8 +1,7 @@
 import 'dotenv/config';
 import { REST, Routes } from 'discord.js';
 import { data as setupCommand } from './commands/setup.js';
-import { data as setup5Command } from './commands/setup5.js';
-import { data as setup10Command } from './commands/setup10.js';
+import { data as setup2Command } from './commands/setup2.js';
 import { data as modCommand } from './commands/mod.js';
 
 const token = process.env.TOKEN;
@@ -18,7 +17,7 @@ if (!stagingGuildId) {
   process.exit(1);
 }
 
-const commands = [setupCommand, setup5Command, setup10Command, modCommand].map((x) => x.toJSON());
+const commands = [setupCommand, setup2Command, modCommand].map((x) => x.toJSON());
 const rest = new REST({ version: '10' }).setToken(token);
 
 try {
