3136e4df7bd8bae2d35cc5ca7b3279f8fdd57d87
David Tennyson <136488774+SteveReporting@users.noreply.github.com>
2026-09-07 23:51:44 +0100

    Deploy only UIpgrade and mod commands

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,8 +1,6 @@
 import 'dotenv/config';
 import { REST, Routes } from 'discord.js';
-import { data as setupCommand } from './commands/setup.js';
-import { data as setup2Command } from './commands/setup2.js';
-import { data as setup3Command } from './commands/setup3.js';
+import { data as uiUpgradeCommand } from './commands/uipgrade.js';
 import { data as modCommand } from './commands/mod.js';
 
 const token = process.env.TOKEN;
@@ -15,9 +13,7 @@ if (!token) {
 }
 
 const commands = [
-  setupCommand.toJSON(),
-  setup2Command.toJSON(),
-  setup3Command.toJSON(),
+  uiUpgradeCommand.toJSON(),
   modCommand.toJSON()
 ];
 const rest = new REST({ version: '10' }).setToken(token);
